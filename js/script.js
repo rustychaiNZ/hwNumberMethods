62533fe1698d0d781ef62802d73e9367af859458
Russell Johnson <rusljohn@gmail.com>
2019-10-16 01:02:22 +1300

    tutroial created for class

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('Number Methods');
+
+var eg1 = '10.39564 68 years';
+var eg2 = '74.38234 45 months';
+
+
+document.getElementById('ri1').innerHTML += parseInt(eg1);
+document.getElementById('ri2').innerHTML += parseFloat(eg2);
